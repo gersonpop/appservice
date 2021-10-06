26c2b03364de37f585acae99cfe167fbfb889496
Maicol Stiven <Maicol Stiven@DESKTOP-MEGODSB>
2021-10-06 06:46:19 -0500

    C10: Requisicion

---
+++ src/main/java/Models/Requisicion.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Models;
+
+/**
+ *
+ * @author Maicol Stiven
+ */
+public class Requisicion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // ATRIBUTOS
+        private String elemento;
+        private int idRequerimiento;
+        private int idCootizacion;
+        private int idOrdenServicio;
+        private String fecha_solicitud;
+        private String fecha_cotizacion;
+        private String fecha_aprobacion;
+        private String fecha_ejecucion;
+        private String fecha_informe;
+        private String descripcion;
+        
+        //CONSTRUCTOR
+        
+        
+    }
+
+}
